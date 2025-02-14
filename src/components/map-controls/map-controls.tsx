12685afcfa9a934d5d2e610ae75a231c94934d7e
Ruslan <ruslan.bayramov03@gmail.com>
2025-02-15 00:56:33 +0800

    feat(zoom-icons): changed zoom buttons to provided icons with styles

--- src/components/map-controls/map-controls.tsx
+++ src/components/map-controls/map-controls.tsx
@@ -1,5 +1,7 @@
 import "./map-controls.css";
 import type { FC } from "react";
+import minusIcon from "@/assets/img/minus.png";
+import plusIcon from "@/assets/img/add.png";
 
 interface ZoomControlsProps {
   onZoomIn: () => void;
@@ -11,11 +13,11 @@ export const ZoomControls: FC<ZoomControlsProps> = ({
   onZoomOut,
 }) => (
   <div className={"map-controls"}>
-    <button onClick={onZoomIn} className={"zoom-btn"}>
-      Zoom In
-    </button>
     <button onClick={onZoomOut} className={"zoom-btn"}>
-      Zoom Out
+      <img className={"zoom-icon"} src={minusIcon.src} alt="Zoom Out" width={44} height={44} />
+    </button>
+    <button onClick={onZoomIn} className={"zoom-btn"}>
+      <img className={"zoom-icon"} src={plusIcon.src} alt="Zoom In" width={44} height={44} />
     </button>
   </div>
 );
